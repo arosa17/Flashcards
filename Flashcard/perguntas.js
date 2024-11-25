57a881d021adb3af4ed24dbc1fd127dacebbeb4b
arosa17 <168016076+arosa17@users.noreply.github.com>
2024-11-25 09:26:35 -0300

    Add files via upload

---
+++ Flashcard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o termo denominado para chuvas de morros?',
+    'Chuvas orográficas'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável local?',
+    'É uma variável que é definida dentro de uma função e só pode ser acessada dentro dessa função'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual é a gramática utilizada para nos expressarmos no passado?',
+    'Past simple'
+)
